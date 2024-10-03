ce3319ec05d23c8927e4c6c80feb817fcebe888a
Suman <sumnsmnt@gmail.com>
2024-10-03 14:09:37 +0530

    updated logic and added comments

--- Day2/tip-calculator.py
+++ Day2/tip-calculator.py
@@ -1,23 +1,25 @@
-#
-name = input("Hi, What's your name?")
+# Welcome message
+name = input("Hi, What's your name? ")
+print("Welcome to Tip Calcualtor", name)
 
-#
-print("Welcome to Tip Calcualtor ", name)
-
-#
+# We have to take 3 inputs from user - total bill amount, tip percentage & no of people 
 bill = float(input("What is the total bill? \n"))
+tip = int(input("What percentage of tip would you like to give? enter number only :) \n"))
+person = int(input("How many person will split the bill? \n"))
 
-#
-tip = int(input("What percentage of tip would you like to give? 10, 15, or 20? \n"))
+# I have to find out bill per person, so start with total tip, then add the bill with it 
+# and then devide the total bill with no of person
+total_tip = bill * (tip / 100)
+total_bill = bill + total_tip
+bill_per_person = round(total_bill / person, 2)
 
-#
-person = int(input("How many person to split the bill? \n"))
+print(f"Each person should pay: ${bill_per_person}")
 
-#
-tip_as_percent = tip /100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / person
-final_amount = round(bill_per_person, 2)
+# We can also format the round function as the round function will not print 2 decimal 
+# values if there is no value in floating point till the 2 decimal point.
+# That's why we we use format() function. It will print upto n decimal (what we menton in the line)
+# even if there is no actual floating point value.
 
-print(f"Each person should pay: ${final_amount}")
+# per_person_amount = total_bill / person
+# final_amount = "{:.2f}".format(per_person_amount)
+# print(f"Each person should pay: ${final_amount}")
