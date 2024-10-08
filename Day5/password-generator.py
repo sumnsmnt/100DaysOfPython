6e6223b8e4048a1cb8bb51d78e5add857284c49c
Suman <sumnsmnt@gmail.com>
2024-10-08 20:15:57 +0530

    fix the alphabets

--- Day5/password-generator.py
+++ Day5/password-generator.py
@@ -4,18 +4,11 @@
 import random
 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']   
-lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
-                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 
-                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
-                     'z'] 
-  
-upper_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  
-                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q', 
-                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
-                     'Z'] 
-  
-symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',  
-           '*', '(', ')', '<'] 
+lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+upper_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
+symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')', '<'] 
 
 print("Welcome to Python Password Generator!")
 # takes input from user
