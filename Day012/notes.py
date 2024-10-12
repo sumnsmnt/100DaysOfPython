5e97074424ff006a82057e93919e1df9998966b7
Suman <sumnsmnt@gmail.com>
2024-10-12 22:10:54 +0530

    added notes of Day 12

---
+++ Day012/notes.py
@@ -0,0 +1,27 @@
+############ Namespaces: Local vs Global ############
+
+# # Global Scope
+# player_health = 10
+# def drink_potion():
+#     #Local Scope
+#     potion_strength = 2
+#     print(player_health)
+# drink_potion()
+
+
+# enemies = "1 Enemy"
+# def increase_enemies():
+#     enemies = "2 Enemies"
+#     print(f"enemies inside function: {enemies}")
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+
+# # There is no Block Scope in Python
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alien"]
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+#     print(new_enemy)
+
