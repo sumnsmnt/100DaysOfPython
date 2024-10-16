bcf71a83e16b27217a6c575391e4cd762a422839
Suman <sumnsmnt@gmail.com>
2024-10-16 22:47:28 +0530

    added notes of Day 16

---
+++ Day016/notes.py
@@ -0,0 +1,13 @@
+####### Object Oriented Programming #######
+from turtle import Turtle, Screen
+tom = Turtle()
+print(tom)
+tom.shape("turtle")
+tom.color("red")
+tom.forward(111)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+# import prettytable import PrettyTable
