92563ddccdd4d945d916591ee665682bde4fe536
Suman <sumnsmnt@gmail.com>
2024-10-19 21:56:53 +0530

    updated the program

--- Day010/function-calculator.py
+++ Day010/function-calculator.py
@@ -23,14 +23,29 @@ operations = {
     "*": multiply,
     "/": divide,
 }
+def calculator():
+    num1 = float(input("What is the first number? "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the second number? "))
 
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the second number? "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from above line: ")
+        calculation_function = operations[operation_symbol]
+        result = calculation_function(num1, num2)
 
-calculation_function = operations[operation_symbol]
-result = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
 
-print(f"{num1} {operation_symbol} {num2} = {result}")
+        recalculation = input(f'''
+        Type 'r' to recalculate with {result}, 
+        type 'y' to continue with new calculation,
+        or type 'n' to exit. ''')
+        if  recalculation == "r":
+            num1 = result
+        elif  recalculation == "y":
+            calculator()
+        else:
+            should_continue = False
+
+calculator()
