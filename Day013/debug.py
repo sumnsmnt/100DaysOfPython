e7919e4bf8adb9f327218744b112bcf3ab4bf7ab
Suman <sumnsmnt@gmail.com>
2024-10-25 22:27:20 +0530

    add debugging exersise

---
+++ Day013/debug.py
@@ -0,0 +1,37 @@
+# ######### Odd or Even bug fixing ########
+
+# number = int(input("What number to check? "))
+
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+
+# ######## Leap Year bug fixing ########
+
+# year = int(input("Enter year to check: "))
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is a leap year.")
+#         else:
+#             print(f"{year} is not leap year.")
+#     else:
+#         print(f"{year} is a leap year.")
+# else:
+#     print(f"{year} is not leap year.")
+
+
+# ######## FizzBuzz bug fixing ########
+
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
