a8a0f104b1a0b11be72579dcffedd0dfb6acc35e
Suman <sumnsmnt@gmail.com>
2024-10-28 23:10:47 +0530

    add notes of Day 19

---
+++ Day019/notes.py
@@ -0,0 +1,46 @@
+# from turtle import Turtle, Screen
+
+# the_turtle = Turtle()
+# screen = Screen()
+
+# def move_forwards():
+#     the_turtle.forward(10)
+
+# screen.listen()
+# screen.onkey(key = "space", fun = move_forwards)
+# screen.exitonclick()
+
+
+# Etch A Scatch - Create a drawing manually
+# from turtle import Turtle, Screen
+
+# the_turtle = Turtle()
+# screen = Screen()
+
+# the_turtle.pensize(5)
+
+# def move_forwards():
+#     the_turtle.forward(10)
+
+# def move_backwards():
+#     the_turtle.backward(10)
+
+# def turn_left():
+#     the_turtle.left(10)
+
+# def turn_right():
+#     the_turtle.right(10)
+
+# def clear_screen():
+#     the_turtle.clear()
+#     the_turtle.penup()
+#     the_turtle.home()
+#     the_turtle.pendown()
+
+# screen.listen()
+# screen.onkey(move_forwards, "w")
+# screen.onkey(move_backwards, "s")
+# screen.onkey(turn_left, "a")
+# screen.onkey(turn_right, "d")
+# screen.onkey(clear_screen, "c")
+# screen.exitonclick()
